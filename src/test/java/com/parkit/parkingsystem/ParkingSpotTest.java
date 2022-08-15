e489f2715e1c213299d9c1f4e04e042946ca1337
Bouteveille jerome <formation@community-technologie.fr>
2022-08-15 17:40:02 +0200

    Adding tests for up coverage rate

---
+++ src/test/java/com/parkit/parkingsystem/ParkingSpotTest.java
@@ -0,0 +1,31 @@
+package com.parkit.parkingsystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+
+public class ParkingSpotTest {
+	
+	ParkingSpot parkingSpot;
+	
+	@Test
+	public void setIdParkingSpotTest() {
+		parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+		parkingSpot.setId(2);
+		assertEquals(2, parkingSpot.getId());
+	}
+	
+	@Test
+	public void setTypeParkingSpotTest() {
+		parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+		parkingSpot.setParkingType(ParkingType.BIKE);
+		assertEquals(ParkingType.BIKE, parkingSpot.getParkingType());
+	}
+	
+
+}
